62163929d81c716b118de08e12662626fede7563
javier yucra yauri <javier_mejor20@hotmail.com>
2020-05-08 23:10:02 -0500

    carrito1.solojava

--- MiAsistenteOnline/MiAsistenteOnline.Web/wwwroot/js/site.js
+++ MiAsistenteOnline/MiAsistenteOnline.Web/wwwroot/js/site.js
@@ -2,18 +2,96 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+
+    var NombreProductos = new Array();
+    var Precio = new Array();
+    var Cantidad = new Array();
+    var Subtotal = new Array();
+
+var contador = 0;
+var total = 0; 
+
+
+
+
+
 function getDataAjax(GrupoArticulo, action) {
     $.ajax({
         type: "get",
         url: action,
-        data: { GrupoArticulo },
-        success: function (response) {
-            $("#productosCategoria").html(response);
-        }
-
+        data: { GrupoArticulo
     }
+    }) // Se ejecuta si todo fue bien.
+        .done(function (result) {
+            $("#productosCategoria").html(result);
+        })
+        // Se ejecuta si se produjo un error.
+        .fail(function (xhr, status, error) {
 
-    );
+        })
+        // Hacer algo siempre, haya sido exitosa o no.
+        .always(function () {
+
+        });
 
 }
 
+$(function () {
+    $("#IrProductos").click(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: "@Url.Action('BusProductos', 'Products')", // Url
+            data: {
+                GrupoArticulo: $("#IrProductos").val()// Parámetros
+            },
+            type: "get"  // Verbo HTTP
+        })
+            // Se ejecuta si todo fue bien.
+            .done(function (result) {
+                $("#productosCategoria").html(result);
+            })
+            // Se ejecuta si se produjo un error.
+            .fail(function (xhr, status, error) {
+
+            })
+            // Hacer algo siempre, haya sido exitosa o no.
+            .always(function () {
+
+            });
+    });
+});
+
+
+
+function agregarcarrito(nombre, precio, elemento) {
+
+    var nombreA = elemento + "A";
+    var cantidad = document.getElementById(nombreA).value;
+    alert( "SE añadio a carrito " );
+
+    NombreProductos[contador] = nombre;
+    Precio[contador] = precio;
+    Cantidad[contador] = cantidad;
+    Subtotal[contador] = (parseFloat(cantidad).toFixed(2) * parseFloat(precio).toFixed(2)).toFixed(2);
+    contador = contador + 1;
+
+    var cadena = '';
+    total = 0;
+    for (var i = 0; i < contador; i++) {
+
+        total = (parseFloat(total) + parseFloat(Subtotal[i]));
+     
+        cadena = cadena + '<tr><th scope = "col" >' + NombreProductos[i] + '</th ><th scope="col">' + Precio[i] + '</th><th scope="col">' + Cantidad[i] + '</th><th scope="col">' + Subtotal[i] + '</th></tr >';
+    } 
+
+    var element = document.getElementById("descripcionCompra");
+    
+    element.innerHTML = cadena;
+    document.getElementById("total").innerHTML = "Total : S/."+total.toString();
+
+
+}
